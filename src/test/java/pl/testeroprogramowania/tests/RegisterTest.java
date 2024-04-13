dda8dd1ecb7723416f0c182a5f365b293157d316
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-04-13 21:06:31 +0200

    add getDashBoardLink and  assertion

--- src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
+++ src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
@@ -1,5 +1,7 @@
 package pl.testeroprogramowania.tests;
 
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.testeroprogramowania.pages.HomePage;
 
@@ -9,8 +11,13 @@ public class RegisterTest extends BaseTest{
     public void registerUserTest() {
 
         int randomNumber = (int) (Math.random()*1000);
-        new HomePage(driver).openMyAccoutPage()
-                .registerUser("test"+randomNumber+"@test.pl","haslotestowe");
+        WebElement entryTitle = new HomePage(driver).openMyAccoutPage()
+                .registerUser("test"+randomNumber+"@test.pl","haslotestowe")
+                .getDashboardLink();
+
+        Assert.assertTrue(entryTitle.isDisplayed());
+        Assert.assertEquals(entryTitle.getText(),"Dashboard");
+
 
 
 

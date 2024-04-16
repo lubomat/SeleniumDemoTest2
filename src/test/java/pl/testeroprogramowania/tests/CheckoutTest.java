9758a801b7742c8c7ca2e36e61e813bef1147f13
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-04-16 11:11:42 +0200

    add assertions to CheckoutTest

--- src/test/java/pl/testeroprogramowania/tests/CheckoutTest.java
+++ src/test/java/pl/testeroprogramowania/tests/CheckoutTest.java
@@ -1,7 +1,10 @@
 package pl.testeroprogramowania.tests;
 
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.testeroprogramowania.pages.HomePage;
+import pl.testeroprogramowania.pages.OrderDetailsPage;
 import pl.testeroprogramowania.pages.models.Customer;
 
 public class CheckoutTest extends BaseTest {
@@ -12,12 +15,18 @@ public class CheckoutTest extends BaseTest {
         customer.setEmail("specificemail@test.pl");
 
 
-        new HomePage(driver)
+        OrderDetailsPage orderDetailsPage = new HomePage(driver)
                 .openShopPage()
                 .openProduct("Java Selenium WebDriver")
                 .addProductToCart()
                 .viewCart()
                 .openAddressDetails()
                 .fillAddressDetails(customer,"some random comment");
+
+        Assert.assertEquals(orderDetailsPage.getOrderNotice()
+                .getText(),"Thank you. Your order has been received.");
+
+        Assert.assertEquals(orderDetailsPage.getProductName()
+                .getText(),"Java Selenium WebDriver × 1");
     }
 }
